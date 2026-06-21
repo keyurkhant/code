549fbc87236326958151f79ea228cf84e102f903
Keyur Khant <kpkhant007@yahoo.com>
2026-06-21 15:39:07 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let l = 0;
+        let r = numbers.length - 1;
+
+        while (l < r) {                        
+            const total = numbers[l] + numbers[r];
+
+            if (total == target) {
+                return [l+1, r+1];
+            } else if (total > target) {
+                r--;
+            } else {
+                l++;
+            }
+
+            // if( numbers[l] >= target) l++;
+            // if( numbers[r] >= target) r--;
+
+        }
+        return [];
+    }
+}
