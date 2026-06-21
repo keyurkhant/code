9059edbca51009710ee20005401e80faa168ff2c
Keyur Khant <kpkhant007@yahoo.com>
2026-06-21 15:18:26 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let l = 0;
+        let r = s.length - 1;
+
+        const regex = /^[a-zA-Z0-9]+$/;
+
+        while (l < r) {
+            if(!regex.test(s[l])){
+                l++;
+                continue;
+            } 
+            if(!regex.test(s[r])) {
+                r--;
+                continue;
+            } 
+
+            if(s[l].toLowerCase() == s[r].toLowerCase()) {
+                l++;
+                r--;
+            } else {
+                console.log("Why false",l , r, s[l], s[r]);
+                return false;
+            }
+        }
+
+        return true
+    }
+}
